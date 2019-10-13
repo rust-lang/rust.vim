rust-lang/rust.vim

commit 33c3e214a606414baaeed9e06b4e410c4f473409

Support format range

autoload/rustfmt.vim
@@ -20,17 +20,20 @@ endif
 
 let s:got_fmt_error = 0
 
-function! rustfmt#Format()
-  let l:curw = winsaveview()
-  let l:tmpname = expand("%:p:h") . "/." . expand("%:p:t") . ".rustfmt"
-  call writefile(getline(1, '$'), l:tmpname)
+function! s:RustfmtCommandRange(filename, line1, line2)
+  let l:arg = {"file": shellescape(a:filename), "range": [a:line1, a:line2]}
+  return printf("%s %s --write-mode=overwrite --file-lines '[%s]'", g:rustfmt_command, g:rustfmt_options, json_encode(l:arg))
+endfunction
 
-  let command = g:rustfmt_command . " --write-mode=overwrite "
+function! s:RustfmtCommand(filename)
+  return g:rustfmt_command . " --write-mode=overwrite " . g:rustfmt_options . " " . shellescape(a:filename)
+endfunction
 
+function! s:RunRustfmt(command, curw, tmpname)
   if exists("*systemlist")
-    let out = systemlist(command . g:rustfmt_options . " " . shellescape(l:tmpname))
+    let out = systemlist(a:command)
   else
-    let out = split(system(command . g:rustfmt_options . " " . shellescape(l:tmpname)), '\r\?\n')
+    let out = split(system(a:command), '\r\?\n')
   endif
 
   if v:shell_error == 0 || v:shell_error == 3
@@ -38,7 +41,7 @@ function! rustfmt#Format()
     try | silent undojoin | catch | endtry
 
     " Replace current file with temp file, then reload buffer
-    call rename(l:tmpname, expand('%'))
+    call rename(a:tmpname, expand('%'))
     silent edit!
     let &syntax = &syntax
 
@@ -76,8 +79,28 @@ function! rustfmt#Format()
     let s:got_fmt_error = 1
     lwindow
     " We didn't use the temp file, so clean up
-    call delete(l:tmpname)
+    call delete(a:tmpname)
   endif
 
-  call winrestview(l:curw)
+  call winrestview(a:curw)
+endfunction
+
+function! rustfmt#FormatRange(line1, line2)
+  let l:curw = winsaveview()
+  let l:tmpname = expand("%:p:h") . "/." . expand("%:p:t") . ".rustfmt"
+  call writefile(getline(1, '$'), l:tmpname)
+
+  let command = s:RustfmtCommandRange(l:tmpname, a:line1, a:line2)
+
+  call s:RunRustfmt(command, l:curw, l:tmpname)
+endfunction
+
+function! rustfmt#Format()
+  let l:curw = winsaveview()
+  let l:tmpname = expand("%:p:h") . "/." . expand("%:p:t") . ".rustfmt"
+  call writefile(getline(1, '$'), l:tmpname)
+
+  let command = s:RustfmtCommand(l:tmpname)
+
+  call s:RunRustfmt(command, l:curw, l:tmpname)
 endfunction

ftplugin/rust.vim
@@ -140,6 +140,9 @@ command! -range=% RustPlay :call rust#Play(<count>, <line1>, <line2>, <f-args>)
 " See |:RustFmt| for docs
 command! -buffer RustFmt call rustfmt#Format()
 
+" See |:RustFmtRange| for docs
+command! -range -buffer RustFmtRange call rustfmt#FormatRange(<line1>, <line2>)
+
 " Mappings {{{1
 
 " Bind ⌘R in MacVim to :RustRun

commit 8293adcd9c5645379133bea4d77de30b1476528c

Only remove undo point if s:RunRustfmt is executed from BufWritePre
Otherwise, we remove unintended undo points.

We can use the already existing parameter to figure out whether we are
executing from BufWritePre context.

Fixes #358. Issue existed since #33c3e214a606, introduced by #111.

autoload/rustfmt.vim
@@ -110,7 +110,7 @@ function! s:DeleteLines(start, end) abort
     silent! execute a:start . ',' . a:end . 'delete _'
 endfunction
 
-function! s:RunRustfmt(command, tmpname, fail_silently)
+function! s:RunRustfmt(command, tmpname, from_writepre)
     mkview!
 
     let l:stderr_tmpname = tempname()
@@ -147,8 +147,10 @@ function! s:RunRustfmt(command, tmpname, fail_silently)
 
     let l:open_lwindow = 0
     if v:shell_error == 0
-        " remove undo point caused via BufWritePre
-        try | silent undojoin | catch | endtry
+        if a:from_writepre
+            " remove undo point caused via BufWritePre
+            try | silent undojoin | catch | endtry
+        endif
 
         if a:tmpname ==# ''
             let l:content = l:out
@@ -168,7 +170,7 @@ function! s:RunRustfmt(command, tmpname, fail_silently)
             call setloclist(0, [])
             let l:open_lwindow = 1
         endif
-    elseif g:rustfmt_fail_silently == 0 && a:fail_silently == 0
+    elseif g:rustfmt_fail_silently == 0 && !a:from_writepre
         " otherwise get the errors and put them in the location list
         let l:errors = []
 
@@ -222,12 +224,12 @@ function! rustfmt#FormatRange(line1, line2)
     let l:tmpname = tempname()
     call writefile(getline(1, '$'), l:tmpname)
     let command = s:RustfmtCommandRange(l:tmpname, a:line1, a:line2)
-    call s:RunRustfmt(command, l:tmpname, 0)
+    call s:RunRustfmt(command, l:tmpname, v:false)
     call delete(l:tmpname)
 endfunction
 
 function! rustfmt#Format()
-    call s:RunRustfmt(s:RustfmtCommand(), '', 0)
+    call s:RunRustfmt(s:RustfmtCommand(), '', v:false)
 endfunction
 
 function! rustfmt#Cmd()
@@ -255,7 +257,7 @@ function! rustfmt#PreWrite()
         return
     endif
 
-    call s:RunRustfmt(s:RustfmtCommand(), '', 1)
+    call s:RunRustfmt(s:RustfmtCommand(), '', v:true)
 endfunction
 
 